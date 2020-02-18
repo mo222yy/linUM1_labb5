a80dc8c0cf323843dab1c00819339bf28a23d0e8
mo222yy <mo222yy@student.lnu.se>
2020-02-18 16:35:07 +0100

    modified main.c

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-	printf("The first four letters of the alphabet:\n");
+	printf("The LAST four letters of the alphabet:\n");
 	print_A();
 	print_B();
 	print_C();
